9c898dddf2e8765bad7aaaa16ef57a51e49a0c1c
Azer Kerimov <azerkerimov03@gmail.com>
2021-11-27 01:26:10 +0400

    first

---
+++ Homework first/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10; int i = 2; int count = 0;
+            if (n <= 1)
+            {
+                Console.WriteLine("invalid number");
+             }
+            else
+            {
+                for (i = 2; i <= n/2; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        Console.WriteLine("{0} is not a prime number",n);
+                        count++;
+                        break;
+                    }
+                }
+                if (count == 0) 
+                {
+                    Console.WriteLine("{0} is a prime number",n);
+                        }
+            }
+        }
+    }
+}
