f4613d740358a8a0ec0c1481e866e4a5daa07d65
MayilKer <94452375+MayilKer@users.noreply.github.com>
2021-11-27 15:34:32 +0400

    Add files via upload

---
+++ HomeWork 6/HomeWork 6/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region 2-ci tapwiriq
+
+//int number = 156;
+//int result = 0;
+//int reminder = 0;
+//while (number> 0)
+//{
+//    reminder = number % 10;
+//    result = result + reminder;
+//    number = number / 10;
+
+//}
+//Console.WriteLine(result);
+#endregion
+#region 3-cu tapwiriq
+//int month = 6;
+//switch (month)
+//{
+//    case 12:
+//        case 1:
+//        case 2:
+//        Console.WriteLine("Winter");
+//        break;
+//        case 3:
+//        case 4:
+//        case 5:
+//        Console.WriteLine("Spring");
+//        break;
+//    case 6:
+//        case 7:
+//        case 8:
+//        Console.WriteLine("Summer");
+//        break;
+//    case 9:
+//    case 10:
+//        case 11:
+//        Console.WriteLine("Autumn");
+//        break;
+//        default:
+//        Console.WriteLine("Ilde 12 ay var");
+//        break;  
+
+//}
+#endregion
