ff4059f45ada86539dd89863d35a52a67682f518
MayilKer <94452375+MayilKer@users.noreply.github.com>
2021-12-03 01:29:29 +0400

    Add files via upload

---
+++ HomeWork9/HomeWork9/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork9
+{
+    class Students
+    {
+        //public string Name;
+        //public string Surname;
+        //public int Age;
+
+
+        //public Students(string name, string surname, int age) : this(age)
+        //{
+        //    Name = name;
+        //    Surname = surname;
+        //    Age = age;
+        //}
+        //public Students(int age)
+        //{
+        //    Age = age;
+        //}
+
+
+        //public void FullName(string name,string surname,int age)
+        //{
+        //    Console.WriteLine($"{name} {surname} {age}");
+        //}
+    }
+}
